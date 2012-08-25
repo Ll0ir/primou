996840d85fdf9d2117483de27c5f5ea20ece278f
Rob Shannon <spirit2457@gmail.com>
2012-08-25 12:37:18 +0100

    1st

---
+++ BoardConfig.mk
@@ -0,0 +1,125 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from common msm7x30
+-include device/htc/msm7x30-common/BoardConfigCommon.mk
+
+# inherit from the proprietary version
+-include vendor/htc/primou/BoardConfigVendor.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := primou
+
+BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=primou no_console_suspend=1
+BOARD_KERNEL_BASE := 0x14300000
+BOARD_KERNEL_PAGE_SIZE := 4096
+
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+TARGET_USE_SCORPION_PLD_SET := true
+TARGET_SCORPION_BIONIC_PLDOFFS := 6
+TARGET_SCORPION_BIONIC_PLDSIZE := 128
+
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := primou
+BOARD_VENDOR_QCOM_AMSS_VERSION := 1200
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+TARGET_PROVIDES_LIBLIGHTS := true
+#BOARD_USES_LEGACY_RIL := true
+
+BOARD_HAVE_HTC_FFC := true
+BOARD_USE_REVERSE_FFC := true
+
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DSCREEN_RELEASE -DREFRESH_RATE=60 -DICS_CAMERA_BLOB -DRIL_VERSION_2_SUPPORT -DQCOM_NO_SECURE_PLAYBACK
+
+BOARD_PREBUILT_LIBAUDIO := false
+BOARD_USES_QCOM_AUDIO_VOIPMUTE := false
+BOARD_USES_QCOM_AUDIO_RESETALL := false
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+
+
+# Wifi related defines
+WIFI_BAND                        := 802_11_ABG
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE                := bcmdhd
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+
+# cat /proc/emmc
+#dev:        size     erasesize name
+#mmcblk0p17: 00040000 00000200 "misc"
+#mmcblk0p21: 0087f400 00000200 "recovery"
+#mmcblk0p22: 00400000 00000200 "boot"
+#mmcblk0p25: 22dffe00 00000200 "system"
+#mmcblk0p27: 12bffe00 00000200 "cache"
+#mmcblk0p26: 496ffe00 00000200 "userdata"
+#mmcblk0p28: 014bfe00 00000200 "devlog"
+#mmcblk0p29: 00040000 00000200 "pdata"
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 585101312
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1232072704
+BOARD_BOOTIMAGE_PARTITION_SIZE := 4194304
+BOARD_FLASH_BLOCK_SIZE := 262144
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+
+TARGET_KERNEL_SOURCE := kernel/htc/msm7x30
+TARGET_KERNEL_CONFIG := cyanogen_primou_defconfig
+TARGET_PREBUILT_KERNEL := device/htc/primou/prebuilt/root/kernel
+
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+#BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+BOARD_USES_MMCUTILS := false
+BOARD_HAS_NO_MISC_PARTITION := false
+
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_otg/msm_hsusb/gadget/lun0/file
+
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+#TESTING
+TARGET_USES_PMEM := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_SF_BYPASS := true
+TARGET_HAVE_BYPASS := true
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
+
+# RIL
+BOARD_USE_NEW_LIBRIL_HTC := true
---
+++ cm.mk
@@ -0,0 +1,22 @@
+$(call inherit-product, device/htc/primou/full_primou.mk)
+
+PRODUCT_RELEASE_NAME := One_V
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_primou BUILD_FINGERPRINT=htc_europe/htc_primou/primou:4.1.1/JRO03L/330937:user/release-keys PRIVATE_BUILD_DESC="4.1.1 JRO03L 330937 release-keys"
+
+PRODUCT_NAME := cm_primou
+PRODUCT_DEVICE := primou
+
+PRODUCT_PACKAGES += \
+    LegacyCamera
+
+# Gallery2 replacement - QuickPic
+PRODUCT_COPY_FILES +=  \
+    device/htc/primou/prebuilt/app/QuickPic.apk:system/app/Gallery2.apk \
+    device/htc/primou/prebuilt/lib/libqpicjni88.so:system/lib/libqpicjni88.so
